2115d1d03b6c87484e44714d07edb21f0163bace
nbvrivnf <alfredkaito23@gmail.com>
2023-05-08 15:29:36 +0900

    Initial commit

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range a Technologies in the web devlopment world.
+      From Back-end To Design
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-end</ListTitle>
+          <ListParagraph>
+            Experrince with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-end</ListTitle>
+          <ListParagraph>
+            Experrince with <br />
+            Node.js and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experrince with <br />
+            tools liks Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
+);
+
+export default Technologies;
---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'MERN Memories',
+    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+
+    tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://nbvrivnf.github.io/Project_React2/',
+    visit: 'https://nbvrivnf.github.io/Project_React2/',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce',
+    description: "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+
+    tags: ['React', 'JavaScript'],
+    source: 'https://nbvrivnf.github.io/My-Profile/',
+    visit: 'https://nbvrivnf.github.io/My-Profile/',
+    id: 1,
+  },
+  {
+    title: 'WebRTC App',
+    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+
+    tags: ['React', 'WebRTC'],
+    source: 'https://fronted-mentor-challenge-pink.vercel.app/',
+    visit: 'https://fronted-mentor-challenge-pink.vercel.app/',
+    id: 2,
+  },
+  {
+    title: 'Unichat',
+    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+
+    tags: ['React', 'ChatEngine', 'Firebase'],
+    source: 'https://nbvrivnf.github.io/kissa__cafe/',
+    visit: 'https://nbvrivnf.github.io/kissa__cafe/',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
