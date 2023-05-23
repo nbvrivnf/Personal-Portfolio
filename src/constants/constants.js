323453578cfc620247c5b9667fef106eb020abb1
nbvrivnf <alfredkaito23@gmail.com>
2023-05-23 10:10:49 +0900

    変更の説明

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,36 +2,36 @@ export const projects = [
   {
     title: 'MERN Memories',
     description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-
+    image: '/images/10.png',
     tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://nbvrivnf.github.io/Project_React2/',
+    source: 'https://github.com/nbvrivnf/Project_React2',
     visit: 'https://nbvrivnf.github.io/Project_React2/',
     id: 0,
   },
   {
     title: 'E-Commerce',
     description: "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-
+    image: '/images/11.png',
     tags: ['React', 'JavaScript'],
-    source: 'https://nbvrivnf.github.io/My-Profile/',
+    source: 'https://github.com/nbvrivnf/My-Profile',
     visit: 'https://nbvrivnf.github.io/My-Profile/',
     id: 1,
   },
   {
     title: 'WebRTC App',
     description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-
+    image: '/new_images/13.png',
     tags: ['React', 'WebRTC'],
-    source: 'https://fronted-mentor-challenge-pink.vercel.app/',
+    source: 'https://vercel.com/nbvrivnf/fronted-mentor-challenge',
     visit: 'https://fronted-mentor-challenge-pink.vercel.app/',
     id: 2,
   },
   {
     title: 'Unichat',
     description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-
+    image: '/images/12.png',
     tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://nbvrivnf.github.io/kissa__cafe/',
+    source: 'https://github.com/nbvrivnf/kissa__cafe',
     visit: 'https://nbvrivnf.github.io/kissa__cafe/',
     id: 3,
   },
